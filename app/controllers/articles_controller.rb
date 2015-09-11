a248cd250c394f9cbc44e51824e9a4b13e9f1165
gVien <wavetech86@gmail.com>
2015-09-11 14:31:00 -0700

    update article controller for create and add show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,27 @@
 class ArticlesController < ApplicationController
-  # Interesting that new can be comment out but Rails still
-  # renders the new.html.erb
-  def new
+
+  def show #3
+    @article = Article.find(params[:id])
   end
 
-  def create
-    render plain: params[:article].inspect
+  # Interesting that new can be comment out but Rails still
+  # renders the new.html.erb
+  def new # 1 for reference
   end
+
+
+  # p params will return this format
+  # {"utf8"=>"✓", "authenticity_token"=>"JL7IJi2/OKRORu/WpGFjM9bBkxIioq9Szhdx4srALcpajYc7cmuF6UUGkDM0erX+UVqQwx2vQmgZwW+o/R0ErQ==", "article"=>{"title"=>"sdfdsfdsf", "text"=>"sdfsfds"}, "commit"=>"Save Article", "controller"=>"articles", "action"=>"create"}
+  def create  #2
+    @article = Article.new(article_params)
+
+    @article.save
+    redirect_to @article
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
+
