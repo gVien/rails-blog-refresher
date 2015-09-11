e14cf5adbe4d79b656212a02ab436f5817c8db84
gVien <wavetech86@gmail.com>
2015-09-11 14:12:30 -0700

    create migration and updated article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,10 @@
 class ArticlesController < ApplicationController
+  # Interesting that new can be comment out but Rails still
+  # renders the new.html.erb
   def new
+  end
 
+  def create
+    render plain: params[:article].inspect
   end
 end
