f086f6e707d2c67c9a084c1bb23b6f4954a19945
gVien <wavetech86@gmail.com>
2015-09-11 18:23:23 -0700

    add comments controller, update show.html.erb to add comment box

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,15 @@
+class CommentsController < ApplicationController
+  def create
+    # find the article
+    @article = Article.find(params[:article_id])
+    #create the comment for that specific article
+    @comment = @article.comments.create(comment_params)
+    # redirect to the specific article path
+    redirect_to article_path(@article)
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
+end
