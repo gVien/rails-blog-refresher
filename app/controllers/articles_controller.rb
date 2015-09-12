c9ca36d6de5eb6eb17ed6d9f725674ccacd389cc
gVien <wavetech86@gmail.com>
2015-09-11 17:15:15 -0700

    add edit and update methods in controller and add edit view file

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,6 +13,9 @@ class ArticlesController < ApplicationController
     @article = Article.new  # 5, form still renders to new without this but without this, the errors message will be nil (if error is added in the show.html.erb)
   end
 
+  def edit  #6 this finds the id and the object of Article. We need an update method to update it
+    @article = Article.find(params[:id])
+  end
 
   # p params will return this format
   # {"utf8"=>"✓", "authenticity_token"=>"JL7IJi2/OKRORu/WpGFjM9bBkxIioq9Szhdx4srALcpajYc7cmuF6UUGkDM0erX+UVqQwx2vQmgZwW+o/R0ErQ==", "article"=>{"title"=>"sdfdsfdsf", "text"=>"sdfsfds"}, "commit"=>"Save Article", "controller"=>"articles", "action"=>"create"}
@@ -23,6 +26,16 @@ class ArticlesController < ApplicationController
       redirect_to @article
     else
       render "new"
+    end
+  end
+
+  def update  #7, after edit method
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render "edit"
     end
   end
 
